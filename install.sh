6f350ccdce5ea3821aa521a2ad6cd2344868f98f
ChaYoung You <yousbe@gmail.com>
2013-04-09 19:55:57 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd $( dirname "$0" ) && pwd )"
+mv ~/.screenrc ~/.screenrc.old
+ln -s $DIR/screenrc ~/.screenrc
+mv ~/.vim ~/.vim.old
+ln -s $DIR/vimfiles/ftplugin ~/.vim/ftplugin
+mv ~/.vimrc ~/.vimrc.old
+ln -s $DIR/vimrc ~/.vimrc
+mv ~/.zshrc ~/.zshrc.old
+ln -s $DIR/zshrc ~/.zshrc
