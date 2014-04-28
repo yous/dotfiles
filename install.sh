fddfc287d577742192ef481fbd1df28fa0d062a2
ChaYoung You <yousbe@gmail.com>
2014-04-29 04:30:21 +0900

    Add instruction messages

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ function replace_file()
 {
   if [ -h "$HOME/.$1" ]; then
     rm "$HOME/.$1"
+    ln -s "$DIR/$1" "$HOME/.$1"
+    echo "Updated ~/.$1"
   elif [ -e "$HOME/.$1" ]; then
     mv "$HOME/.$1" "$HOME/.$1.old"
+    echo "Renamed ~/.$1 to ~/.$1.old"
+    ln -s "$DIR/$1" "$HOME/.$1"
+    echo "Created ~/.$1"
   fi
-  ln -s "$DIR/$1" "$HOME/.$1"
 }
 
 for FILENAME in "gitignore" "irbrc" "oh-my-zsh" "screenrc" "tmux.conf" "vimrc" "zshrc"
 do
   replace_file $FILENAME
 done
+echo "Done."
