dec69f26fc8b1beecc00a2112a01ce3f32679d8f
ChaYoung You <yousbe@gmail.com>
2014-04-29 04:47:35 +0900

    Add echoerr() for stderr

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 DIR="$( cd $( dirname "$0" ) && pwd )"
 
+function echoerr()
+{
+  echo "$@" 1>&2
+}
+
 function replace_file()
 {
   if [ -h "$HOME/.$1" ]; then
     if rm "$HOME/.$1" && ln -s "$DIR/$1" "$HOME/.$1"; then
       echo "Updated ~/.$1"
     else
-      echo "Failed to update ~/.$1"
+      echoerr "Failed to update ~/.$1"
     fi
   elif [ -e "$HOME/.$1" ]; then
     if mv "$HOME/.$1" "$HOME/.$1.old"; then
@@ -15,10 +20,10 @@ function replace_file()
       if ln -s "$DIR/$1" "$HOME/.$1"; then
         echo "Created ~/.$1"
       else
-        echo "Failed to create ~/.$1"
+        echoerr "Failed to create ~/.$1"
       fi
     else
-      echo "Failed to rename ~/.$1 to ~/.$1.old"
+      echoerr "Failed to rename ~/.$1 to ~/.$1.old"
     fi
   fi
 }
