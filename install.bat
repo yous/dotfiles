6b0774481a011d16c2f8423ce001c3e6d76f06a1
ChaYoung You <yousbe@gmail.com>
2015-01-26 18:51:08 +0900

    Implement basic install.bat
    Link `gitconfig`, `gitignore_global`, `vimrc` by default.

--- install.bat
+++ install.bat
@@ -1,1 +1,25 @@
-mklink %HOMEDRIVE%%HOMEPATH%\.vimrc %~dp0vimrc
+@echo off
+
+:: Files to link
+set files=gitconfig^
+
+gitignore_global^
+
+vimrc
+
+:: Main
+for %%a in (%files%) do (
+  call :replaceFile %%a
+)
+goto :EOF
+
+:: Functions
+:replaceFile
+set dest=%USERPROFILE%\.%1
+mklink %dest% %~dp0%1
+if %errorlevel% == 0 (
+  echo Created %dest%
+) else (
+  echo Failed to create %dest%
+)
+goto :EOF
