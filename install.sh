82e14d09a6096d01232c3735c7f320db48aea99b
Chayoung You <yousbe@gmail.com>
2015-09-16 07:34:11 +0900

    Use tpm and tmux-yank plugin

--- install.sh
+++ install.sh
@@ -78,6 +78,7 @@ case "$1" in
     do
       replace_file "$FILENAME"
     done
+    replace_file 'tpm' '.tmux/plugins/tpm'
     for FILENAME in bin/*
     do
       replace_file "$FILENAME" "$FILENAME"
