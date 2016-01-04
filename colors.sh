ad09a67b630e8090318228c1e43dd7f0749a45b5
Chayoung You <yousbe@gmail.com>
2016-01-04 17:34:57 +0900

    colors.sh: Update display layouts

--- colors.sh
+++ colors.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-for i in {0..255} ; do
-  printf "\x1b[38;5;${i}mcolour${i}\n"
+for i in {0..7}; do
+  next=$((${i} + 8))
+  printf "\x1b[38;5;${i}mcolour${i} \x1b[38;5;${next}mcolour${next}\n"
+done
+for i in {16..255}; do
+  printf "\x1b[38;5;${i}mcolour${i} "
+  if [[ $((${i} % 6)) == 3 ]]; then
+    printf "\n"
+  fi
 done
