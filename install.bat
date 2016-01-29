172f3d332cb1b448b225d167307db1db4941f3c0
Chayoung You <yousbe@gmail.com>
2016-01-29 14:54:49 +0900

    Parse the second argument also

--- install.bat
+++ install.bat
@@ -21,7 +21,13 @@ goto :EOF
 
 :: Functions
 :replaceFile
-set dest=%USERPROFILE%\.%1
+if not "%2" == "" (
+  set dest=%USERPROFILE%\%2
+) else (
+  set dest=%USERPROFILE%\.%1
+)
+for %%f in ("%dest%") do set dirname=%%~dpf
+if not exist %dirname% mkdir %dirname%
 mklink %dest% %~dp0%1
 if %errorlevel% == 0 (
   echo Created %dest%
