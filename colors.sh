c24d9c36ccb8e64fcc76ea4203c976bc4dc519f7
Chayoung You <yousbe@gmail.com>
2017-03-17 16:31:23 +0900

    colors.sh: Fix ShellCheck issues

--- colors.sh
+++ colors.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 for i in {0..7}; do
-  next=$((${i} + 8))
-  printf "\x1b[38;5;${i}mcolour${i} \x1b[38;5;${next}mcolour${next}\n"
+  next=$((i + 8))
+  printf "\x1b[38;5;%dmcolour%d \x1b[38;5;%dmcolour%d\n" "${i}" "${i}" "${next}" "${next}"
 done
 for i in {16..255}; do
-  printf "\x1b[38;5;${i}mcolour${i} "
-  if [[ $((${i} % 6)) == 3 ]]; then
+  printf "\x1b[38;5;%dmcolour%d " "${i}" "${i}"
+  if [[ $((i % 6)) == 3 ]]; then
     printf "\n"
   fi
 done
