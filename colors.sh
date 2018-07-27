f5c68267f41624ce8304f46970b48d80480f01e8
Chayoung You <yousbe@gmail.com>
2018-07-27 15:31:52 +0900

    colors.sh: Escape backslash explicitly
    This fix SC1117.

--- colors.sh
+++ colors.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 for i in {0..7}; do
   next=$((i + 8))
-  printf "\x1b[38;5;%dmcolour%d \x1b[38;5;%dmcolour%d\n" "${i}" "${i}" "${next}" "${next}"
+  printf "\\x1b[38;5;%dmcolour%d \\x1b[38;5;%dmcolour%d\\n" "${i}" "${i}" "${next}" "${next}"
 done
 for i in {16..255}; do
-  printf "\x1b[38;5;%dmcolour%d " "${i}" "${i}"
+  printf "\\x1b[38;5;%dmcolour%d " "${i}" "${i}"
   if [[ $((i % 6)) == 3 ]]; then
-    printf "\n"
+    printf "\\n"
   fi
 done
