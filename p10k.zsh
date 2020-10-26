4cdf8247489c48d58e16b1d5e8eada2710560df4
Chayoung You <yousbe@gmail.com>
2020-10-26 16:38:38 +0900

    p10k.zsh: use cyan for context to match with yous/lime

--- p10k.zsh
+++ p10k.zsh
@@ -815,9 +815,9 @@
   # Context color when running with privileges.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=1
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=4
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=6
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=4
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=6
 
   # Context format when running with privileges: bold user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%B%n@%m'
