c4f6dbf7f3c2f4fb02090d13b09b52fe4a28d925
Chayoung You <yousbe@gmail.com>
2020-11-19 03:11:16 +0900

    colors.sh: add color table for background colors
    This is brought from: https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html

--- colors.sh
+++ colors.sh
@@ -9,3 +9,18 @@ for i in {16..255}; do
     printf "\n"
   fi
 done
+
+# https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+T='gYw'
+printf "\n                 40m     41m     42m     43m     44m     45m     46m     47m\n"
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+           '  36m' '1;36m' '  37m' '1;37m'; do
+  FG=${FGs// /}
+  printf " %s \033[%s  %s  " "$FGs" "$FG" "$T"
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
+    printf "%s \033[%s\033[%s  %s  \033[0m" "$EINS" "$FG" "$BG" "$T"
+  done
+  echo
+done
+echo
