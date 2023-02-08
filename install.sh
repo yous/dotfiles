b55c7ce7356a8a1a629036874b42538c33a0e775
Chayoung You <yousbe@gmail.com>
2023-02-08 20:37:51 +0900

    install.sh: update installation script for ruby-install

--- install.sh
+++ install.sh
@@ -198,9 +198,9 @@ case "$1" in
     if [ "$(uname)" = 'Darwin' ]; then
       brew install ruby-install
     else
-      wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
-      tar -xzvf ruby-install-0.7.0.tar.gz
-      cd ruby-install-0.7.0/
+      wget -O ruby-install-0.9.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.9.0.tar.gz
+      tar -xzvf ruby-install-0.9.0.tar.gz
+      cd ruby-install-0.9.0/
       sudo make install
     fi
     ;;
